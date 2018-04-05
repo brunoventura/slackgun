e67ed1754d5d05038a9bbbef0ac21321e546706a
Bruno Ventura <brun0ventur4@gmail.com>
2018-04-05 13:43:56 -0300

    fix port

--- src/index.js
+++ src/index.js
@@ -17,6 +17,6 @@ app.use('/send', async function(req, res) {
     }
 })
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
 
 module.exports = app;
